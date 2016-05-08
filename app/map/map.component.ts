2c17a72d702ccef3ee3cb02a68a18578ed26959e
jigkoxsee <jigko@jigko.me>
2016-05-08 15:32:08 +0700

    ggmap work

--- app/map/map.component.ts
+++ app/map/map.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, Renderer,ElementRef} from '@angular/core';
-
+import { Component, OnInit, ViewChild, Renderer, ElementRef} from '@angular/core';
+import { MapService } from './map.service';
 declare var google: any;
 
 @Component({
@@ -10,21 +10,30 @@ declare var google: any;
     #map {
       height: 300px;
     }
-  `]
+  `],
+  providers:[MapService]
 })
 export class MapComponent implements OnInit {
     myMap: any;
     @ViewChild('map') divMap: ElementRef;
+    
+    direction :any;
+    
 
-    constructor(public renderer: Renderer) { }
+    constructor(public renderer: Renderer, private mapService: MapService) { }
 
     ngOnInit() {
-
     }
 
     ngAfterViewInit() {
+        console.log(this.mapService.getDirection().subscribe(dir => this.direction = dir));
+        console.log(this.direction);
+        /*
+          this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+         */
         console.log(this.divMap.nativeElement);
-        this.myMap= new google.maps.Map(this.divMap.nativeElement, { center: {lat: -34.397, lng: 150.644}, scrollwheel: false, zoom: 8 });
+        this.myMap = new google.maps.Map(this.divMap.nativeElement, { center: { lat: -34.397, lng: 150.644 }, scrollwheel: false, zoom: 8 });
     }
 
 }
